4f19c872151ea1189d3d6c11f1bd7fc2ce0b70aa
razwi1 <razwimkofficial@gmail.com>
2025-09-24 22:13:54 +0400

    slight adjustments for vercel deployment_ v5

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -7,7 +7,7 @@ import thankYouVideo from '../assets/videos/thank_you.mp4'; // Import the local 
 export default function Landing() {
   const [form, setForm] = useState({ name: '', email: '', phone: '' });
   const [submitted, setSubmitted] = useState(false);
-  const banner = "./public/banner.jpeg";
+  const banner = "./assets/banner.jpeg";
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setForm({ ...form, [e.target.name]: e.target.value });
