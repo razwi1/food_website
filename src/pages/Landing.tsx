4eed9e40a4a91d2e2941c76cdbce0d3ddcd2c568
razwi1 <razwimkofficial@gmail.com>
2025-09-24 23:17:17 +0400

    patch 0.0.1

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,7 @@
 ﻿// src/pages/Landing.tsx
 import { useState } from 'react';
 import { supabase } from '../supabaseClient';
-import banner from 'src/assets/banner.jpeg';
+import banner from '../assets/banner.jpeg';
 import thankYouVideo from '../assets/videos/thank_you.mp4'; // Import the local video file
 
 export default function Landing() {
